fcb1e66a8070d053c3f5b84f8229ff9273e6e70d
jscriptural <isongjohn014@gmail.com>
2023-11-11 21:46:35 +0100

    update this

--- redlit.js
+++ redlit.js
@@ -8,6 +8,9 @@ redlit.math = math;
 redlit.object = object;
 redlit.set = set;
 
-;
+let setA = new Set([1,2,3,4,5]);
+let setB = new Set([3,4,5,6,7,8,8, 9,0]);
+
+console.log(redlit.set.union(setB,setA))
 Object.freeze(redlit);
 export default redlit;
