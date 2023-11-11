06ed710a92f59d7203a6c107b5d7a5449e73d944
jscriptural <isongjohn014@gmail.com>
2023-11-11 21:45:03 +0100

    Added some set utilities functions

--- src/set/set.js
+++ src/set/set.js
@@ -1,8 +1,16 @@
 import {union} from "./union.js";
+import {intersection} from "./intersection.js";
+import {difference} from "./difference.js";
+import {isSubset} from "./isSubset.js";
+import {isSuperset} from "./isSuperset.js";
 
 export const set = {};
 
 set.union = union;
+set.intersection = intersection;
+set.difference = difference;
+set.isSuperset = isSuperset;
+set.isSubset = isSubset;
 
 
 Object.freeze(set);
